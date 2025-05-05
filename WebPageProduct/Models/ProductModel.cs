93f5757b92eeb56be91f25d952aa08f604470bd0
Ratthakit <ratthakit.k@ku.th>
2025-05-05 23:33:17 +0700

    Add project files.

---
+++ WebPageProduct/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebPageProduct.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+
+    public class ProductModelV2
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
